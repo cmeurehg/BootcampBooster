41b7ba04222eb65c859f7f5d9c84046bf904906c
Claude Meurehg <claude.meurehg@mail.utoronto.ca>
2018-08-02 19:04:41 -0400

    git needs a push

---
+++ routes/html-routes.js
@@ -0,0 +1,24 @@
+var path = require("path");
+
+module.exports = function(app) {
+
+  // index route loads view.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/partials/test.html"));
+  });
+
+//   // cms route loads cms.html
+//   app.get("/cms", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/cms.html"));
+//   });
+
+//   // blog route loads blog.html
+//   app.get("/blog", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/blog.html"));
+//   });
+
+//   app.get("/authors", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/author-manager.html"));
+//   });
+
+};
