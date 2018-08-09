7151f4cf5340ad07743922774243cd7bfa64699a
Claude Meurehg <claude.meurehg@mail.utoronto.ca>
2018-08-09 15:14:37 -0400

    x

--- routes/activity-routes.js
+++ routes/activity-routes.js
@@ -2,16 +2,6 @@
 //TODO::Make this look similar to skills.js
 module.exports = (app, db) => {
   
-<<<<<<< HEAD
-  app.get('/activiti', (req, res) => {
-    db.activity.findAll()
-      .then(skills => {
-        res.json(skills);
-      });
-  });
-
-  app.get('/activity/:id', (req, res) => {
-=======
   app.get('/activity', (req, res) => {
     db.activity.findAll()
       .then(activity => {
@@ -19,25 +9,15 @@ module.exports = (app, db) => {
       });
   });
    app.get('/activity/:id', (req, res) => {
->>>>>>> ce3f05bb3ff083801bde21bd8cfecea738703e11
     const id = req.params.id;
     db.activity.find({
       where: { id: id}
     })
-<<<<<<< HEAD
-      .then(skill => {
-        res.json(skill);
-      });
-  });
-
-  app.post('/activity', (req, res) => {
-=======
       .then(activity => {
         res.json(activity);
       });
   });
    app.post('/activity', (req, res) => {
->>>>>>> ce3f05bb3ff083801bde21bd8cfecea738703e11
     console.log(' :) ', req.body)
     const name = req.body.name;
     const week_id = req.body.week_id; //AJAX call from the front, grabbing data from the request.body  make the call....
@@ -47,36 +27,16 @@ module.exports = (app, db) => {
       week_id: week_id,
       type: type
     })
-<<<<<<< HEAD
-      .then(newSkill => {
-      res.json(newSkill);
-    });
-  });
-
-  app.patch('/activity/:id', (req, res) => {
-=======
       .then(newActivity => {
       res.json(newActivity);
     });
   });
    app.patch('/activity/:id', (req, res) => {
->>>>>>> ce3f05bb3ff083801bde21bd8cfecea738703e11
     const id = req.params.id;
     const updates = req.body.updates;
     db.activity.find({
       where: { id: id }
     })
-<<<<<<< HEAD
-      .then(skill => {
-        return skill.updateAttributes(updates);
-      })
-      .then(updatedSkill => {
-        res.json(updatedSkill);
-      });
-  });
-
-  app.delete('/skill/:id', (req, res) => {
-=======
       .then(activity => {
         return activity.updateAttributes(updates);
       })
@@ -85,22 +45,12 @@ module.exports = (app, db) => {
       });
   });
    app.delete('/activity/:id', (req, res) => {
->>>>>>> ce3f05bb3ff083801bde21bd8cfecea738703e11
     const id = req.params.id;
     db.activity.destroy({
       where: { id: id }
     })
-<<<<<<< HEAD
-      .then(deletedSkill => {
-        res.json(deletedSkill);
-      });
-  });
-
-};
-=======
       .then(deletedActivity => {
         res.json(deletedActivity);
       });
   });
  }; 
->>>>>>> ce3f05bb3ff083801bde21bd8cfecea738703e11
